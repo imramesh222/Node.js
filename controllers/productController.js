762c6b16def0d6b69161f79e7f64364c62e6569c
imramesh222 <imrameshrawat@gmail.com>
2025-01-27 16:52:30 +0545

    productController

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,11 +1,15 @@
 const ProductModel=require('./modules/productModel')
 ports.addProduct=async(req,res)=>{
+  if(!req.file){
+    return res.status(400).json({error:"File is required"})
+  }
   let productToAdd=new ProductModel({
     poduct_title:req.body.product_title,
     poduct_price:req.body.product_price,
     poduct_description:req.body.product_description,
     count_in_stock:req.body.count_in_stock,
-    category:req.body.catrgory
+    category:req.body.catrgory,
+    produc_image:req.file.path
 
   })
   productToAdd=await productToAdd.save()
